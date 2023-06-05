738b922730d394d4bce8743eba20c643535f676a
brianrono <brianrono16@gmail.com>
2023-06-05 23:39:15 +0300

    Added files to work on

--- Gym-Buddy-frontend/src/App.jsx
+++ Gym-Buddy-frontend/src/App.jsx
@@ -1,13 +1,35 @@
-import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    fetch('')
+      .then((response) => response.json())
+      .then((data) => setExercises(data));
+  }, []);
 
   return (
-    <>
-
-    </>
-  )
+    <div>
+      <h1>Fitness Tracker</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exercises.map((exercise) => (
+            <tr key={exercise.id}>
+              <td>{exercise.name}</td>
+              <td>{exercise.duration} minutes</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
