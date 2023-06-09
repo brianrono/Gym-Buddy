9bc7aa275a4d4c862a7d1aad47a45a0501338022
brianrono <brianrono16@gmail.com>
2023-06-09 09:59:18 +0300

    Updated home page display

--- Gym-Buddy-frontend/src/components/Home.jsx
+++ Gym-Buddy-frontend/src/components/Home.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <div className="home-container">
-        <h1>Welcome to Your Fitness Buddy</h1>
-        <h2>Featured Trainer</h2>
-        <p>John Doe</p>
-        <p><i>Specializes in strength training and weightlifting</i>.</p>
-        <h2>Recent Reviews</h2>
-        <p>EXCELLENT PERSONAL TRAINER.</p>
-        <p>AMAZING GYM, AMAZING WORKOUTS!</p>
-        <p>THE BEST ENERGY TO MATCH YOUR DAY</p>
-        </div>
-    );
-}
+        <>
+            <div className="home-container">
+                <h1>Welcome to Your Fitness Buddy</h1>
+                <h2>Featured Trainer</h2>
+                <p>John Doe</p>
+                <p><i>Specializes in strength training and weightlifting</i>.</p>
+            </div>
+    
+            <Link to="/dashboard">
+                <button className='home-button'>Proceed to Appointments</button>
+            </Link>
+    
+            <div className='review-container'>
+                <h2>Recent Reviews</h2>
+                <p>EXCELLENT PERSONAL TRAINER.</p>
+                <p>AMAZING GYM, AMAZING WORKOUTS!</p>
+                <p>THE BEST ENERGY TO MATCH YOUR DAY</p>
+            </div>
+        </>
+        );
+    }
 
 export default Home;
